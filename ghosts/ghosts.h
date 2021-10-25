e83c7742ade5965c8b94cffbe7666632fe12935d
alfeijo28 <92060010+alfeijo28@users.noreply.github.com>
2021-10-25 17:45:36 -0300

    Update ghosts.h

--- ghosts/ghosts.h
+++ ghosts/ghosts.h
@@ -1,9 +1,8 @@
 void ghosts_move(unsigned char &, unsigned char &, unsigned char &, unsigned char &, unsigned char &, unsigned char &, 
-	unsigned char &, unsigned char &, bool [], bool [], bool [], bool []);
-void ghosts_redraw(unsigned char &, unsigned char &, unsigned char &, unsigned char &, unsigned char &, unsigned char &, 
-	unsigned char &, unsigned char &, bool [], bool [], bool [], bool [], ALLEGRO_BITMAP *, ALLEGRO_BITMAP *, ALLEGRO_BITMAP *, ALLEGRO_BITMAP *);
+	unsigned char &, unsigned char &, bool [], bool [], bool [], bool [], bool &, bool &, bool &, bool &, int, int);
 bool possible(bool [], bool [], unsigned char);
-void move(unsigned char &, unsigned char , bool [], short int &);
+void move(unsigned char &, unsigned char , bool [], bool &);
 void looking(bool [], char []);
-bool killed(unsigned char &, unsigned char &, unsigned char &, unsigned char &, unsigned char &, unsigned char &, 
-	unsigned char &, unsigned char &, int, int);
+bool killed(unsigned char, unsigned char, unsigned char, unsigned char, unsigned char, unsigned char, unsigned char, unsigned char, int, int);
+void kill(unsigned char &, unsigned char &, unsigned char &, unsigned char &, unsigned char &, unsigned char &, unsigned char &, unsigned char &, 
+	int, int, bool &, bool &, bool &, bool &);
