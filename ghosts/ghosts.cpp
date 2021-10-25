7b9e2101e2cc4e5bf515ff5146826ff16f6b2656
alfeijo28 <92060010+alfeijo28@users.noreply.github.com>
2021-10-25 17:46:00 -0300

    Update ghosts.cpp

--- ghosts/ghosts.cpp
+++ ghosts/ghosts.cpp
@@ -25,57 +25,6 @@ unsigned char ENEMIES_MAP[17][17] ={
 	"1111111111111111"
 };
 
-void looking(bool movement[], char img[]){
-	if(movement[0] == true) strcpy(img, "brocolis2.tga");
-	else if(movement[1] == true) strcpy(img, "brocolis1.tga");
-	else if(movement[2] == true) strcpy(img, "brocolis4.tga");
-	else if(movement[3] == true) strcpy(img, "brocolis3.tga");
-}
-
-void ghosts_move(unsigned char &posx_1, unsigned char &posy_1, unsigned char &posx_2, unsigned char &posy_2, unsigned char &posx_3, unsigned char &posy_3, 
-	unsigned char &posx_4, unsigned char &posy_4, bool movement_1[], bool movement_2[], bool movement_3[], bool movement_4[]){
-
-	srand (time(NULL));
-
-    move(posx_1, posy_1, movement_1);
-    move(posx_2, posy_2, movement_2);
-    move(posx_3, posy_3, movement_3);
-    move(posx_4, posy_4, movement_4);
-}
-
-void ghosts_redraw(unsigned char &posx_1, unsigned char &posy_1, unsigned char &posx_2, unsigned char &posy_2, unsigned char &posx_3, unsigned char &posy_3, 
-	unsigned char &posx_4, unsigned char &posy_4, bool movement_1[], bool movement_2[], bool movement_3[], bool movement_4[], ALLEGRO_BITMAP *ghost1, 
-	ALLEGRO_BITMAP *ghost2, ALLEGRO_BITMAP *ghost3, ALLEGRO_BITMAP *ghost4){
-
-	char img[] = "brocolis1.tga";
-
-    looking(movement_1, img);
-    ghost1 = al_load_bitmap(img);
-    looking(movement_2, img);
-    ghost2 = al_load_bitmap(img);
-    looking(movement_3, img);
-    ghost3 = al_load_bitmap(img);    
-    looking(movement_4, img);
-    ghost4 = al_load_bitmap(img);
-
-	al_draw_bitmap(ghost1, (int)posx_1, (int)posy_1,0);
-	al_draw_bitmap(ghost2, (int)posx_2, (int)posy_2,0);
-	al_draw_bitmap(ghost3, (int)posx_3, (int)posy_3,0);
-	al_draw_bitmap(ghost4, (int)posx_4, (int)posy_4,0);
-
-}
-
-bool killed(unsigned char &posx_1, unsigned char &posy_1, unsigned char &posx_2, unsigned char &posy_2, unsigned char &posx_3, unsigned char &posy_3, 
-	unsigned char &posx_4, unsigned char &posy_4, int x, int y){
-
-	if(x == posx_1 && y == posy_1) return true;
-	else if(x == posx_2 && y == posy_2) return true;
-	else if(x == posx_3 && y == posy_3) return true;
-	else if(x == posx_4 && y == posy_4) return true;
-
-	return false;
-}
-
 bool possible(bool movs[], bool intention[], unsigned char n){
 	unsigned char oposite;
 
@@ -99,12 +48,13 @@ bool possible(bool movs[], bool intention[], unsigned char n){
 	return true;
 }
 
-void move(unsigned char &posx, unsigned char &posy, bool movs[], short int &started){
+
+void move(unsigned char &posx, unsigned char &posy, bool movs[], bool &started){
 	short int x, y;
 	x = posx/15;
 	y = posy/15;
 
-	if(ENEMIES_MAP[y][x] == '0' && started == 4){
+	if(ENEMIES_MAP[y][x] == '0' && started == true){
 		if(movs[0] == true) posy -= 15;
 		else if(movs[1] == true) posx += 15;
 		else if(movs[2] == true) posy += 15;
@@ -175,11 +125,69 @@ void move(unsigned char &posx, unsigned char &posy, bool movs[], short int &star
 		else if(ENEMIES_MAP[y][x] == '5') posy -= 15;
 		else if(x == 8 && y == 6){
 			posx += 15;
-			started++;
+			started = true;
 		}
 		else if(x == 7 && y == 6){
 			posx -= 15;
-			started++;
+			started = true;
 		}
 	}
 }
+
+void looking(bool movement[], char img[]){
+	if(movement[0] == true) strcpy(img, "brocolis2.tga");
+	else if(movement[1] == true) strcpy(img, "brocolis1.tga");
+	else if(movement[2] == true) strcpy(img, "brocolis4.tga");
+	else if(movement[3] == true) strcpy(img, "brocolis3.tga");
+}
+
+void ghosts_move(unsigned char &posx_1, unsigned char &posy_1, unsigned char &posx_2, unsigned char &posy_2, unsigned char &posx_3, unsigned char &posy_3, 
+	unsigned char &posx_4, unsigned char &posy_4, bool movement_1[], bool movement_2[], bool movement_3[], bool movement_4[], bool &started_1, 
+	bool &started_2, bool &started_3, bool &started_4, int px, int py){
+
+	move(posx_1, posy_1, movement_1, started_1);
+	move(posx_2, posy_2, movement_2, started_2);
+	move(posx_3, posy_3, movement_3, started_3);
+	move(posx_4, posy_4, movement_4, started_4);
+}
+
+bool killed(unsigned char posx_1, unsigned char posy_1, unsigned char posx_2, unsigned char posy_2, unsigned char posx_3, unsigned char posy_3, 
+	unsigned char posx_4, unsigned char posy_4, int x, int y){
+
+	if(x == posx_1 && y == posy_1) return true;
+	if(x == posx_2 && y == posy_2) return true;
+	if(x == posx_3 && y == posy_3) return true;
+	if(x == posx_4 && y == posy_4) return true;
+
+	return false;
+}
+
+void kill(unsigned char &posx_1, unsigned char &posy_1, unsigned char &posx_2, unsigned char &posy_2, unsigned char &posx_3, unsigned char &posy_3, 
+	unsigned char &posx_4, unsigned char &posy_4, int x, int y, bool &started_1, bool &started_2, bool &started_3, bool &started_4){
+
+	if(x == posx_1 && y == posy_1){
+		posy_1 = 105;
+		posx_1 = 90;
+		started_1 = false;
+	}
+
+	if(x == posx_2 && y == posy_2){
+		posy_2 = 105;
+		posx_2 = 105;
+		started_2 = false;
+	}
+
+	if(x == posx_3 && y == posy_3) {
+		posy_3 = 105;
+		posx_3 = 120;
+		started_3 = false;
+	}
+
+	if(x == posx_4 && y == posy_4) {
+		posy_4 = 105;
+		posx_4 = 135;
+		started_4 = false;
+	}
+}
+
+
